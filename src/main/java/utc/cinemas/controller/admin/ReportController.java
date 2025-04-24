a9ba0e64248d79976239da1682ffd7fcf1b85b50
thaidp <thai.doan@vconnex.vn>
2025-04-24 09:56:34 +0700

    Reports Management

---
+++ src/main/java/utc/cinemas/controller/admin/ReportController.java
@@ -0,0 +1,12 @@
+package utc.cinemas.controller.admin;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/admin/reports")
+public class ReportController {
+
+}
